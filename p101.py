5f90095a5c29281b8483278703835a8f232586bf
Mauli1357 <71227691+Mauli1357@users.noreply.github.com>
2023-06-22 19:50:13 -0400

    Add files via upload

---
+++ p101.py
@@ -0,0 +1,44 @@
+import random
+no = random.randint(1,6)
+yorn =  input("Enter y or n: ")
+def timer(sec):
+    while yorn=="y":
+        no = random.randint(1,6)
+        
+
+if no ==1:
+    print("{     }")
+    print("{     }")
+    print("{  *  }")
+    print("{     }")
+    print("{     }")
+if no ==2:
+    print("{     }")
+    print("{ *   }")
+    print("{     }")
+    print("{   * }")
+    print("{     }")
+if no ==3:
+    print("{     }")
+    print("{ *   }")
+    print("{  *  }")
+    print("{   * }")
+    print("{     }")
+if no ==4:
+    print("{     }")
+    print("{ * * }")
+    print("{     }")
+    print("{ * * }")
+    print("{     }")
+if no ==5:
+    print("{     }")
+    print("{ * * }")
+    print("{  *  }")
+    print("{ * * }")
+    print("{     }")
+if no ==6:
+    print("{     }")
+    print("{ * * }")
+    print("{ * * }")
+    print("{ * * }")
+    print("{     }")
